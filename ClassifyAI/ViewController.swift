758626a121280e894f93966f33728cc300c13d22
daiki0730 <xcrqf313@yahoo.cp.jp>
2021-07-24 22:57:17 +0900

    first commit

---
+++ ClassifyAI/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  ClassifyAI
+//
+//  Created by Nishida Daiki on 2021/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController,
+                      UIImagePickerControllerDelegate,
+                      UINavigationControllerDelegate {
+    @IBOutlet weak var imageDisplay: UIImageView!
+    @IBOutlet weak var predictionDisplay: UITextView!
+    var imagePicker: UIImagePickerController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+    }
+
+    @IBAction func takePhoto(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:
+        [UIImagePickerController.InfoKey : Any]) {
+        imageDisplay.image =
+            info[UIImagePickerController.InfoKey.originalImage] as?
+            UIImage
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+}
+
